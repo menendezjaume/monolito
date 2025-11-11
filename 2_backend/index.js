4834a9d2a7695b9e2f2bac65586c63f03549254d
Miki <mmenendez@colegiosantamonica.eu>
2025-11-11 16:45:37 +0100

    remove views

--- 2_backend/index.js
+++ 2_backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
@@ -20,11 +19,6 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
 });
 
-app.set('view engine', 'ejs');
-
-app.use(express.urlencoded());
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors());
 
 async function initDb() {
@@ -114,118 +108,6 @@ async function initDb() {
         console.error('Error initializing database', error);
     }
 }
-
-app.get('/', (req, res) => {
-    // sql
-    res.render('index', {
-        title: 'Mi super página',
-    });
-});
-
-const isAdmin = (req, res, next) => {
-    if (req.cookies && req.cookies.user && req.cookies.role == 'admin') {
-        return next();
-    }
-    // if is user send to user page
-    if (req.cookies && req.cookies.user && req.cookies.role == 'user') {
-        return res.redirect('/user');
-    }
-    res.redirect('/login');
-};
-
-const isUser = (req, res, next) => {
-    if (req.cookies && req.cookies.user && req.cookies.role == 'user') {
-        return next();
-    }
-    // if is admin send to admin page
-    if (req.cookies && req.cookies.user && req.cookies.role == 'admin') {
-        return res.redirect('/admin');
-    }
-    res.redirect('/login');
-};
-
-// register
-app.get('/register', (req, res) => {
-    res.render('register', {
-        title: 'Register',
-    });
-});
-
-app.post('/register', async (req, res) => {
-    const { user, password } = req.body; // cliente
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await pool.query(
-        'INSERT INTO users (username, password, role) VALUES ($1, $2, $3)',
-        [user, hashedPassword, 'user'],
-    );
-    res.redirect('/login');
-});
-
-// gestion de la vista
-app.get('/login', (req, res) => {
-    res.render('login', {
-        title: 'Login',
-    });
-});
-
-// gestion de los parámetros post
-app.post('/login', async (req, res) => {
-    const { user, password } = req.body; // cliente
-    console.log('Login attempt');
-    console.log('user', user);
-    console.log('password', password);
-
-    const resultado = await pool.query(
-        'SELECT * FROM users WHERE username = $1',
-        [user],
-    );
-    const userdb = resultado.rows[0];
-
-    if (!userdb) {
-        console.log('usuario no existe');
-        return res.status(401).redirect('login');
-    }
-
-    console.log('usuario existe');
-    console.log('userdb.username', userdb.username);
-    console.log('userdb.password', userdb.password);
-    console.log('userdb.role', userdb.role);
-
-    const validPassword = await bcrypt.compare(password, userdb.password);
-
-    if (validPassword) {
-        console.log('usuario y contraseña correcta');
-        res.cookie('user', user); // options - js no secure si
-        res.cookie('role', userdb.role); // options - js no secure si
-        console.log('redirect to ', userdb.role);
-        res.redirect(userdb.role);
-    } else {
-        console.log('contraseña incorrecta');
-        res.status(401).redirect('login');
-    }
-});
-
-app.get('/admin', isAdmin, (req, res) => {
-    // leeriamos el usuario de la cookie
-    // consulta en bbdd del usuario
-    // se lo enviamos por parametro al render
-    res.render('admin', {
-        user: req.cookies.user,
-        title: 'Zona admin privada',
-    });
-});
-
-app.get('/user', isUser, (req, res) => {
-    res.render('user', {
-        user: req.cookies.user,
-    });
-});
-
-app.get('/logout', (req, res) => {
-    res.clearCookie('user');
-    res.clearCookie('role');
-    res.redirect('login');
-});
 
 // public get posts
 app.get('/posts', async (req, res) => {
