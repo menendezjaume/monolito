842ef145b351de0a4b5d8c6c0ff2ed9cfe18f87f
Miki <mmenendez@colegiosantamonica.eu>
2025-11-11 15:09:18 +0100

    3 capas

--- 1_frontend/src/app/app.ts
+++ 1_frontend/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RestApiService } from './services/rest-api.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+
+  posts: any[] = [];
+
+  constructor(private api: RestApiService) { }
+
+  ngOnInit() {
+    this.loadPosts();
+  }
+
+  loadPosts() {
+    this.api.getPosts().subscribe((data) => {
+      this.posts = data;
+    });
+  }
 }
--- 2_backend/index.js
+++ 2_backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
+// picsum-photos
+const { Picsum } = require('picsum-photos');
 
 const app = express();
 const port = 3000;
@@ -22,18 +25,18 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
-function initDb() {
-    pool.connect((err) => {
-        if (err) {
-            console.error('Error connecting to the database', err);
-        } else {
-            console.log('Connected to the database');
-        }
-    });
+async function initDb() {
+    try {
+        await pool.connect();
+        console.log('Connected to the database');
+    } catch (error) {
+        console.error('Error connecting to the database', error);
+    }
     try {
         // create users table if not exists
-        pool.query(
+        await pool.query(
             `CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
@@ -42,6 +45,54 @@ function initDb() {
     )`,
         );
         console.log('Users table created or already exists');
+        // create posts table if not exists
+        pool.query(
+            `CREATE TABLE IF NOT EXISTS posts (
+        id SERIAL PRIMARY KEY,
+        image_url VARCHAR(255),
+        content TEXT NOT NULL,
+        user_id INTEGER REFERENCES users(id)
+    )`,
+        );
+        console.log('Posts table created or already exists');
+        // create default user function
+        const createUser = async (username, password, role) => {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            await pool.query(
+            `INSERT INTO users (username, password, role)
+            VALUES ($1, $2, $3)
+            ON CONFLICT (username) DO NOTHING`,
+            [username, hashedPassword, role],
+            );
+            console.log(`Default user "${username}" created or already exists`);
+        };
+        await createUser('pepe', 'pepe', 'user');
+        createUser('admin', 'admin', 'admin');
+        // create post by user "pepe" if not exists
+        const createPost = async (id, imageUrl, content, username) => {
+            const userResult = await pool.query(
+                `SELECT id FROM users WHERE username = $1`,
+                [username],
+            );
+            if (userResult.rows.length === 0) {
+                console.log(`User "${username}" does not exist. Cannot create post.`);
+                return;
+            }
+            const userId = userResult.rows[0].id;
+            await pool.query(
+            `INSERT INTO posts (id, image_url, content, user_id)
+            VALUES ($1, $2, $3, $4)
+            ON CONFLICT (id) DO NOTHING`,
+            [id, imageUrl, content, userId],
+            );
+            console.log('Post created or already exists');
+        };
+        // create a post
+        await createPost(1, Picsum.url(), 'Este es el contenido del primer post.', 'pepe');
+        // create another post
+        await createPost(2, Picsum.url(), 'Este es el contenido del segundo post.', 'pepe');
+        // create a third post
+        await createPost(3, Picsum.url(), 'Este es el contenido del tercer post.', 'pepe');
     } catch (error) {
         console.error('Error initializing database', error);
     }
@@ -157,6 +208,17 @@ app.get('/logout', (req, res) => {
     res.clearCookie('user');
     res.clearCookie('role');
     res.redirect('login');
+});
+
+// public get posts
+app.get('/posts', async (req, res) => {
+    const resultado = await pool.query(
+        `SELECT posts.id, posts.image_url, posts.content, users.username
+        FROM posts
+        JOIN users ON posts.user_id = users.id`,
+    );
+    const posts = resultado.rows;
+    res.json(posts);
 });
 
 initDb();
